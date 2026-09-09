7cee6a654e2dd9ceb496242dd2ccda8eda0188f7
longuirom <longuirom@gmail.com>
2026-09-09 23:03:13 +0700

    add custom manager
    Signed-off-by: longuirom <longuirom@gmail.com>

--- kernel/manager/apk_sign.c
+++ kernel/manager/apk_sign.c
@@ -366,5 +366,9 @@ bool is_manager_apk(char *path)
 	if (check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588"))
 		return true;
 
+	// Custom
+	if (check_v2_signature(path, 0x375, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549"))
+		return true;
+
 	return false;
 }
